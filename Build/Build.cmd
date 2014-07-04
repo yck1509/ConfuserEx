5409156749ecbeae2576d1147e7ed2a1ea17d047
yck1509 <confuser.net@gmail.com>
2014-07-04 14:39:36 +0800

    Update build script

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -1,8 +1,14 @@
 @echo off
+
+IF NOT EXIST ..\ConfuserEx.snk COPY ..\ConfuserEx_default.snk ..\ConfuserEx.snk
+
 %windir%\microsoft.net\framework\v4.0.30319\msbuild ..\Confuser2.sln /p:Configuration=Release "/p:Platform=Any CPU" /p:DefineConstants="%1"
-@IF %ERRORLEVEL% NEQ 0 GOTO err
+
+IF %ERRORLEVEL% NEQ 0 GOTO err
+
 7z a ConfuserEx_bin.zip -tzip @files.lst
-@exit /B 0
+EXIT /B 0
+
 :err
-@PAUSE
-@exit /B 1
+PAUSE
+EXIT /B 1
