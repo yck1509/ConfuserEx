b8979c152c6890fb382d8be6600eee1fa16a706a
yck1509 <confuser.net@gmail.com>
2014-07-30 22:57:49 +0800

    Add control flow graphs and key sequence

---
+++ Confuser.Core/Helpers/ControlFlowGraph.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using dnlib.DotNet.Emit;
+
+namespace Confuser.Core.Helpers {
+	/// <summary>
+	///     A Control Flow Graph (CFG) of a method
+	/// </summary>
+	public class ControlFlowGraph : IEnumerable<ControlFlowBlock> {
+		private readonly List<ControlFlowBlock> blocks;
+		private readonly CilBody body;
+		private readonly int[] instrBlocks;
+
+		private ControlFlowGraph(CilBody body) {
+			this.body = body;
+			instrBlocks = new int[body.Instructions.Count];
+			blocks = new List<ControlFlowBlock>();
+		}
+
+		/// <summary>
+		///     Gets the number of blocks in this CFG.
+		/// </summary>
+		/// <value>The number of blocks.</value>
+		public int Count {
+			get { return blocks.Count; }
+		}
+
+		/// <summary>
+		///     Gets the <see cref="ControlFlowBlock" /> of the specified id.
+		/// </summary>
+		/// <param name="id">The id.</param>
+		/// <returns>The block with specified id.</returns>
+		public ControlFlowBlock this[int id] {
+			get { return blocks[id]; }
+		}
+
+		IEnumerator<ControlFlowBlock> IEnumerable<ControlFlowBlock>.GetEnumerator() {
+			return blocks.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return blocks.GetEnumerator();
+		}
+
+		/// <summary>
+		///     Gets the block containing the specified instruction.
+		/// </summary>
+		/// <param name="instrIndex">The index of instruction.</param>
+		/// <returns>The block containing the instruction.</returns>
+		public ControlFlowBlock GetContainingBlock(int instrIndex) {
+			return blocks[instrBlocks[instrIndex]];
+		}
+
+		private void PopulateBlockHeaders(HashSet<Instruction> blockHeaders, HashSet<Instruction> handlerHeaders) {
+			for (int i = 0; i < body.Instructions.Count; i++) {
+				Instruction instr = body.Instructions[i];
+
+				if (instr.Operand is Instruction) {
+					blockHeaders.Add((Instruction)instr.Operand);
+					if (i + 1 < body.Instructions.Count)
+						blockHeaders.Add(body.Instructions[i + 1]);
+				} else if (instr.Operand is Instruction[]) {
+					foreach (Instruction target in (Instruction[])instr.Operand)
+						blockHeaders.Add(target);
+					if (i + 1 < body.Instructions.Count)
+						blockHeaders.Add(body.Instructions[i + 1]);
+				} else if ((instr.OpCode.FlowControl == FlowControl.Throw || instr.OpCode.FlowControl == FlowControl.Return) &&
+				           i + 1 < body.Instructions.Count) {
+					blockHeaders.Add(body.Instructions[i + 1]);
+				}
+			}
+			blockHeaders.Add(body.Instructions[0]);
+			foreach (ExceptionHandler eh in body.ExceptionHandlers) {
+				blockHeaders.Add(eh.TryStart);
+				blockHeaders.Add(eh.HandlerStart);
+				blockHeaders.Add(eh.FilterStart);
+				handlerHeaders.Add(eh.HandlerStart);
+				handlerHeaders.Add(eh.FilterStart);
+			}
+		}
+
+		private void SplitBlocks(HashSet<Instruction> blockHeaders, HashSet<Instruction> handlerHeaders) {
+			int nextBlockId = 0;
+			int currentBlockId = -1;
+			Instruction currentBlockHdr = null;
+
+			for (int i = 0; i < body.Instructions.Count; i++) {
+				Instruction instr = body.Instructions[i];
+				if (blockHeaders.Contains(instr)) {
+					if (currentBlockHdr != null) {
+						Instruction footer = body.Instructions[i - 1];
+
+						var type = ControlFlowBlockType.Normal;
+						if (handlerHeaders.Contains(currentBlockHdr) || currentBlockHdr == body.Instructions[0])
+							type |= ControlFlowBlockType.Entry;
+						if (footer.OpCode.FlowControl == FlowControl.Return || footer.OpCode.FlowControl == FlowControl.Throw)
+							type |= ControlFlowBlockType.Exit;
+
+						blocks.Add(new ControlFlowBlock(currentBlockId, type, currentBlockHdr, footer));
+					}
+
+					currentBlockId = nextBlockId++;
+					currentBlockHdr = instr;
+				}
+
+				instrBlocks[i] = currentBlockId;
+			}
+			if (blocks.Count == 0 || blocks[blocks.Count - 1].Id != currentBlockId) {
+				Instruction footer = body.Instructions[body.Instructions.Count - 1];
+
+				var type = ControlFlowBlockType.Normal;
+				if (handlerHeaders.Contains(currentBlockHdr) || currentBlockHdr == body.Instructions[0])
+					type |= ControlFlowBlockType.Entry;
+				if (footer.OpCode.FlowControl == FlowControl.Return || footer.OpCode.FlowControl == FlowControl.Throw)
+					type |= ControlFlowBlockType.Exit;
+
+				blocks.Add(new ControlFlowBlock(currentBlockId, type, currentBlockHdr, footer));
+			}
+		}
+
+		private void LinkBlocks(Dictionary<Instruction, int> indexMap) {
+			for (int i = 0; i < body.Instructions.Count; i++) {
+				Instruction instr = body.Instructions[i];
+				if (instr.Operand is Instruction) {
+					ControlFlowBlock srcBlock = blocks[instrBlocks[i]];
+					ControlFlowBlock dstBlock = blocks[instrBlocks[indexMap[(Instruction)instr.Operand]]];
+					dstBlock.Sources.Add(srcBlock);
+					srcBlock.Targets.Add(dstBlock);
+				} else if (instr.Operand is Instruction[]) {
+					foreach (Instruction target in (Instruction[])instr.Operand) {
+						ControlFlowBlock srcBlock = blocks[instrBlocks[i]];
+						ControlFlowBlock dstBlock = blocks[instrBlocks[indexMap[target]]];
+						dstBlock.Sources.Add(srcBlock);
+						srcBlock.Targets.Add(dstBlock);
+					}
+				}
+			}
+			for (int i = 0; i < blocks.Count; i++) {
+				if (blocks[i].Footer.OpCode.FlowControl != FlowControl.Branch &&
+				    blocks[i].Footer.OpCode.FlowControl != FlowControl.Return &&
+				    blocks[i].Footer.OpCode.FlowControl != FlowControl.Throw) {
+					blocks[i].Targets.Add(blocks[i + 1]);
+					blocks[i + 1].Sources.Add(blocks[i]);
+				}
+			}
+		}
+
+		/// <summary>
+		///     Constructs a CFG from the specified method body.
+		/// </summary>
+		/// <param name="body">The method body.</param>
+		/// <returns>The CFG of the given method body.</returns>
+		public static ControlFlowGraph Construct(CilBody body) {
+			var graph = new ControlFlowGraph(body);
+			if (body.Instructions.Count == 0)
+				return graph;
+
+			var indexMap = new Dictionary<Instruction, int>();
+			for (int i = 0; i < body.Instructions.Count; i++)
+				indexMap.Add(body.Instructions[i], i);
+
+			// Populate block headers
+			var blockHeaders = new HashSet<Instruction>();
+			var handlerHeaders = new HashSet<Instruction>();
+			graph.PopulateBlockHeaders(blockHeaders, handlerHeaders);
+
+			// Split blocks
+			graph.SplitBlocks(blockHeaders, handlerHeaders);
+
+			// Link blocks
+			graph.LinkBlocks(indexMap);
+
+			return graph;
+		}
+	}
+
+	/// <summary>
+	///     The type of Control Flow Block
+	/// </summary>
+	[Flags]
+	public enum ControlFlowBlockType {
+		/// <summary>
+		///     The block is a normal block
+		/// </summary>
+		Normal = 0,
+
+		/// <summary>
+		///     There are unknown edges to this block. Usually used at exception handlers / method entry.
+		/// </summary>
+		Entry = 1,
+
+		/// <summary>
+		///     There are unknown edges from this block. Usually used at filter blocks / throw / method exit.
+		/// </summary>
+		Exit = 2,
+	}
+
+	/// <summary>
+	///     A block in Control Flow Graph (CFG).
+	/// </summary>
+	public class ControlFlowBlock {
+		/// <summary>
+		///     The footer instruction
+		/// </summary>
+		public readonly Instruction Footer;
+
+		/// <summary>
+		///     The header instruction
+		/// </summary>
+		public readonly Instruction Header;
+
+		/// <summary>
+		///     The identifier of this block
+		/// </summary>
+		public readonly int Id;
+
+		/// <summary>
+		///     The type of this block
+		/// </summary>
+		public readonly ControlFlowBlockType Type;
+
+		internal ControlFlowBlock(int id, ControlFlowBlockType type, Instruction header, Instruction footer) {
+			Id = id;
+			Type = type;
+			Header = header;
+			Footer = footer;
+
+			Sources = new List<ControlFlowBlock>();
+			Targets = new List<ControlFlowBlock>();
+		}
+
+		/// <summary>
+		///     Gets the source blocks of this control flow block.
+		/// </summary>
+		/// <value>The source blocks.</value>
+		public IList<ControlFlowBlock> Sources { get; private set; }
+
+		/// <summary>
+		///     Gets the target blocks of this control flow block.
+		/// </summary>
+		/// <value>The target blocks.</value>
+		public IList<ControlFlowBlock> Targets { get; private set; }
+
+		/// <summary>
+		///     Returns a <see cref="System.String" /> that represents this block.
+		/// </summary>
+		/// <returns>A <see cref="System.String" /> that represents this block.</returns>
+		public override string ToString() {
+			return string.Format("Block {0} => {1} {2}", Id, Type, string.Join(", ", Targets.Select(block => block.Id.ToString()).ToArray()));
+		}
+	}
+}
---
+++ Confuser.Core/Helpers/KeySequence.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Confuser.Core.Services;
+
+namespace Confuser.Core.Helpers {
+	/// <summary>
+	///     The type of block in the key sequence
+	/// </summary>
+	public enum BlockKeyType {
+		/// <summary>
+		///     The state key should be explicitly set in the block
+		/// </summary>
+		Explicit,
+
+		/// <summary>
+		///     The state key could be assumed to be same as <see cref="BlockKey.EntryState" /> at the beginning of block.
+		/// </summary>
+		Incremental
+	}
+
+	/// <summary>
+	///     The information of the block in the key sequence
+	/// </summary>
+	public struct BlockKey {
+		/// <summary>
+		///     The state key at the beginning of the block
+		/// </summary>
+		public uint EntryState;
+
+		/// <summary>
+		///     The state key at the end of the block
+		/// </summary>
+		public uint ExitState;
+
+		/// <summary>
+		///     The type of block
+		/// </summary>
+		public BlockKeyType Type;
+	}
+
+	/// <summary>
+	///     Computes a key sequence that is valid according to the execution of the CFG.
+	/// </summary>
+	/// <remarks>
+	///     The caller can utilize the information provided by this classes to instruments state machines.
+	///     For example:
+	///     <code>
+	/// int state = 4;
+	/// for (int i = 0 ; i &lt; 10; i++) {
+	///     state = 6;
+	///     if (i % 2 == 0) {
+	///         state = 3;
+	///     else {
+	///         // The state varaible is guaranteed to be 6 in here.
+	///     }
+	/// }
+	///     </code>
+	/// </remarks>
+	public static class KeySequence {
+		/// <summary>
+		///     Computes a key sequence of the given CFG.
+		/// </summary>
+		/// <param name="graph">The CFG.</param>
+		/// <param name="random">The random source.</param>
+		/// <returns>The generated key sequence of the CFG.</returns>
+		public static BlockKey[] ComputeKeys(ControlFlowGraph graph, RandomGenerator random) {
+			var keys = new BlockKey[graph.Count];
+
+			foreach (ControlFlowBlock block in graph) {
+				var key = new BlockKey();
+				if ((block.Type & ControlFlowBlockType.Entry) != 0)
+					key.Type = BlockKeyType.Explicit;
+				else
+					key.Type = BlockKeyType.Incremental;
+				keys[block.Id] = key;
+			}
+
+			ProcessBlocks(keys, graph, random);
+			return keys;
+		}
+
+		private static void ProcessBlocks(BlockKey[] keys, ControlFlowGraph graph, RandomGenerator random) {
+			uint id = 0;
+			for (int i = 0; i < keys.Length; i++) {
+				keys[i].EntryState = id++;
+				keys[i].ExitState = id++;
+			}
+
+			// Update the state ids with the maximum id
+			bool updated;
+			do {
+				updated = false;
+				foreach (ControlFlowBlock block in graph) {
+					BlockKey key = keys[block.Id];
+					if (block.Sources.Count > 0) {
+						uint newEntry = block.Sources.Select(b => keys[b.Id].ExitState).Max();
+						if (key.EntryState != newEntry) {
+							key.EntryState = newEntry;
+							updated = true;
+						}
+					}
+					if (block.Targets.Count > 0) {
+						uint newExit = block.Targets.Select(b => keys[b.Id].EntryState).Max();
+						if (key.ExitState != newExit) {
+							key.ExitState = newExit;
+							updated = true;
+						}
+					}
+					keys[block.Id] = key;
+				}
+			} while (updated);
+
+			// Replace id with actual values
+			var idMap = new Dictionary<uint, uint>();
+			for (int i = 0; i < keys.Length; i++) {
+				BlockKey key = keys[i];
+
+				uint entryId = key.EntryState;
+				if (!idMap.TryGetValue(entryId, out key.EntryState))
+					key.EntryState = idMap[entryId] = random.NextUInt32();
+
+				uint exitId = key.ExitState;
+				if (!idMap.TryGetValue(exitId, out key.ExitState))
+					key.ExitState = idMap[exitId] = random.NextUInt32();
+
+				keys[i] = key;
+			}
+		}
+	}
+}
