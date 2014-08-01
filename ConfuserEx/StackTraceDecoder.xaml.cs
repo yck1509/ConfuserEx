7d0f5a50aefab51d088ebb7f5342c154006ad746
yck1509 <confuser.net@gmail.com>
2014-08-02 00:45:44 +0800

    Add stack trace decoder UI

---
+++ ConfuserEx/StackTraceDecoder.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using Confuser.Core;
+using Ookii.Dialogs.Wpf;
+
+namespace ConfuserEx {
+	/// <summary>
+	///     Interaction logic for StackTraceDecoder.xaml
+	/// </summary>
+	public partial class StackTraceDecoder : Window {
+
+		public StackTraceDecoder() {
+			InitializeComponent();
+		}
+
+		private readonly Dictionary<string, string> symMap = new Dictionary<string, string>();
+
+		private void PathBox_TextChanged(object sender, TextChangedEventArgs e) {
+			if (File.Exists(PathBox.Text))
+				LoadSymMap(PathBox.Text);
+		}
+
+		private void LoadSymMap(string path) {
+			string shortPath = path;
+			if (path.Length > 35)
+				shortPath = "..." + path.Substring(path.Length - 35, 35);
+
+			try {
+				symMap.Clear();
+				using (var reader = new StreamReader(File.OpenRead(path))) {
+					var line = reader.ReadLine();
+					while (line != null) {
+						int tabIndex = line.IndexOf('\t');
+						if (tabIndex == -1)
+							throw new FileFormatException();
+						symMap.Add(line.Substring(0, tabIndex), line.Substring(tabIndex + 1));
+						line = reader.ReadLine();
+					}
+				}
+				status.Content = "Loaded symbol map from '" + shortPath + "' successfully.";
+			}
+			catch {
+				status.Content = "Failed to load symbol map from '" + shortPath + "'.";
+			}
+		}
+
+		private void ChooseMapPath(object sender, RoutedEventArgs e) {
+			var ofd = new VistaOpenFileDialog();
+			ofd.Filter = "Symbol maps (*.map)|*.map|All Files (*.*)|*.*";
+			if (ofd.ShowDialog() ?? false) {
+				PathBox.Text = ofd.FileName;
+			}
+		}
+
+		private readonly Regex symbolMatcher = new Regex("=[a-zA-Z0-9]+=");
+
+		private void Decode_Click(object sender, RoutedEventArgs e) {
+			var trace = stackTrace.Text;
+			stackTrace.Text = symbolMatcher.Replace(trace, DecodeSymbol);
+		}
+
+		private string DecodeSymbol(Match match) {
+			var sym = match.Value;
+			return symMap.GetValueOrDefault(sym, sym);
+		}
+
+	}
+}
