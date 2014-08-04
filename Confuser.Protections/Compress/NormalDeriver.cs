5eb72b4e8eb0ed0cdd907b4a7baad6d8646fea03
yck1509 <confuser.net@gmail.com>
2014-08-04 23:31:37 +0800

    Randomize compressor operators

--- Confuser.Protections/Compress/NormalDeriver.cs
+++ Confuser.Protections/Compress/NormalDeriver.cs
@@ -11,32 +11,49 @@ namespace Confuser.Protections.Compress {
 		private uint k1;
 		private uint k2;
 		private uint k3;
+		private uint seed;
 
 		public void Init(ConfuserContext ctx, RandomGenerator random) {
 			k1 = random.NextUInt32() | 1;
 			k2 = random.NextUInt32() | 1;
 			k3 = random.NextUInt32() | 1;
+			seed = random.NextUInt32();
 		}
 
 		public uint[] DeriveKey(uint[] a, uint[] b) {
 			var ret = new uint[0x10];
+			var state = seed;
 			for (int i = 0; i < 0x10; i++) {
-				switch (i % 3) {
+				switch (state % 3) {
 					case 0:
-						ret[i] = (a[i] ^ b[i]) + k1;
+						ret[i] = a[i] ^ b[i];
 						break;
 					case 1:
-						ret[i] = (a[i] * b[i]) ^ k2;
+						ret[i] = a[i] * b[i];
 						break;
 					case 2:
-						ret[i] = (a[i] + b[i]) * k3;
+						ret[i] = a[i] + b[i];
 						break;
 				}
+				state = (state * state) % 0x2E082D35;
+				switch (state % 3) {
+					case 0:
+						ret[i] += k1;
+						break;
+					case 1:
+						ret[i] ^= k2;
+						break;
+					case 2:
+						ret[i] *= k3;
+						break;
+				}
+				state = (state * state) % 0x2E082D35;
 			}
 			return ret;
 		}
 
 		public IEnumerable<Instruction> EmitDerivation(MethodDef method, ConfuserContext ctx, Local dst, Local src) {
+			var state = seed;
 			for (int i = 0; i < 0x10; i++) {
 				yield return Instruction.Create(OpCodes.Ldloc, dst);
 				yield return Instruction.Create(OpCodes.Ldc_I4, i);
@@ -46,23 +63,33 @@ namespace Confuser.Protections.Compress {
 				yield return Instruction.Create(OpCodes.Ldloc, src);
 				yield return Instruction.Create(OpCodes.Ldc_I4, i);
 				yield return Instruction.Create(OpCodes.Ldelem_U4);
-				switch (i % 3) {
+				switch (state % 3) {
 					case 0:
 						yield return Instruction.Create(OpCodes.Xor);
+						break;
+					case 1:
+						yield return Instruction.Create(OpCodes.Mul);
+						break;
+					case 2:
+						yield return Instruction.Create(OpCodes.Add);
+						break;
+				}
+				state = (state * state) % 0x2E082D35;
+				switch (state % 3) {
+					case 0:
 						yield return Instruction.Create(OpCodes.Ldc_I4, (int)k1);
 						yield return Instruction.Create(OpCodes.Add);
 						break;
 					case 1:
-						yield return Instruction.Create(OpCodes.Mul);
 						yield return Instruction.Create(OpCodes.Ldc_I4, (int)k2);
 						yield return Instruction.Create(OpCodes.Xor);
 						break;
 					case 2:
-						yield return Instruction.Create(OpCodes.Add);
 						yield return Instruction.Create(OpCodes.Ldc_I4, (int)k3);
 						yield return Instruction.Create(OpCodes.Mul);
 						break;
 				}
+				state = (state * state) % 0x2E082D35;
 				yield return Instruction.Create(OpCodes.Stelem_I4);
 			}
 		}
