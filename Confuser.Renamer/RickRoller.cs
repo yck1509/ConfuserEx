ffd971eab2c56a6ee030e27afe7ac56720d326fa
yck1509 <confuser.net@gmail.com>
2014-12-17 15:31:18 +0800

    Update my present URL

--- Confuser.Renamer/RickRoller.cs
+++ Confuser.Renamer/RickRoller.cs
@@ -9,7 +9,7 @@ namespace Confuser.Renamer {
 	// For my dearest Reflector devs, this is my Christmas present.
 	public static class RickRoller {
 		const string Injection = "\"onclick=\"return(false);\"style=\"background:#ffffff;cursor:default;position:absolute;display:block;width:10000px;height:10000px;top:0px;left:0px\"onmouseover=\"REPL\"></A></TABLE><!--";
-		const string JS = "window.open(\"http://goo.gl/vTJcb4\",\"\",\"fullscreen=yes\")";
+		const string JS = "window.open(\"http://goo.gl/YroZm\",\"\",\"fullscreen=yes\")";
 
 		static string EscapeScript(string script) {
 			return script
